97bfa76b26cc5a15e9336b60b40486c2c91e4fb7
Maximus Champion <maximuschampion2002@gmail.com>
2025-09-04 19:35:46 +0300

    Refactor Book system to follow SOLID principles

---
+++ app/serializer.py
@@ -0,0 +1,25 @@
+import json
+from abc import ABC, abstractmethod
+import xml.etree.ElementTree as Et
+from app.book import Book
+
+
+class Serializer(ABC):
+    @abstractmethod
+    def serialize(self, book: Book) -> str:
+        pass
+
+
+class JsonSerializer(Serializer):
+    def serializer(self, book: Book) -> str:
+        return json.dumps({"title": book.title, "content": book.content})
+
+
+class XmlSerializer(Serializer):
+    def serialize(self, book: Book) -> str:
+        root = Et.Element("book")
+        title = Et.SubElement(root, "title")
+        title.text = book.title
+        content = Et.SubElement(root, "content")
+        content.text = book.content
+        return Et.tostring(root, encoding="utf-8").decode("utf-8")
